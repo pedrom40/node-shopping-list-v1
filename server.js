cf403c259044f4b0000e0b11e5acb2fa5bab3759
pedrom40 <pedro.morin1980@gmail.com>
2017-10-06 08:30:49 -0500

    add recipes model, manually add some recipes, create endpoint to get recipes, add root get to index.html view

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -12,16 +13,29 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-// we're going to add some items to ShoppingList
-// so there's some data to look at
+// we're going to add some items to ShoppingList so there's some data to look at
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
-// when the root of this router is called with GET, return
-// all current ShoppingList items
+// we're going to add some recipies here as is above
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('pancakes', ['pancake mix', 'eggs', 'milk']);
+Recipes.create('toast', ['bread', 'butter']);
+
+// when the root of this router is called with GET, return all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+// when the root of this router is called with GET, return all current Recipes
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+});
+
+// when the root of this app is called, give valid links
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(process.env.PORT || 8080, () => {
